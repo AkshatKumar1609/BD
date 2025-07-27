98eea6bbd7c625e3c1a616aae483138ec58e1335
ranjan kumar <ranjanmurmu2004@gmail.com>
2025-07-27 12:28:37 +0530

    login-Form page

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import InputType from '../../components/shared/Form/InputType'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <>
+       <div className="row">
+        <div className="col-md-8 form-banner">
+            <img src="./assets/images/banner1.jpg" alt="loginImage" />
+        </div>
+        <div className="col-md-4 form-container">
+      <form>
+        <InputType labelText={'Email'} labelFor={'forEmail'} inputType={'email'} name={'email'} />
+
+ 
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+
+        </div>
+       </div>
+
+    </>
   )
 }
 
